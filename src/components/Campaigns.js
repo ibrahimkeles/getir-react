dcb1d9f996e7119a25e8d8e806dbf6036418a7cd
ibrahim <ibokeles0697@gmail.com>
2023-11-16 00:51:34 +0300

    campaings

--- src/components/Campaigns.js
+++ src/components/Campaigns.js
@@ -1,9 +1,40 @@
+import Slider from 'react-slick'
+import Banners from '../api/banners.json'
+import { useState, useEffect } from 'react'
+
+
 
 export default function Campaigns() {
-    return (
-      <div>
-        Campaigns
-      </div>
-    )
+  const [banners, setBanners] = useState([]);
+  useEffect(() => {
+    setBanners(Banners);
+  }, [])
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+
   }
-  
+  return (
+    <div className='container mx-auto max-w-[1233px]'>
+      <h3 className="text-sm font-semibold my-3">Kampanyalar</h3>
+
+      <Slider {...settings} className='-mx-2'>
+        {banners.length && banners.map((banner, index) => (
+          <div key={banner.id}>
+            <picture className='px-2 block'>
+              <img src={banner.image} alt='kampanya görseli' className='rounded' />
+            </picture>
+          </div>
+        ))}
+
+      </Slider>
+    </div>
+  )
+}
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,28 @@
 
+import { useEffect, useState } from "react"
+import categoryData from '../api/categories.json'
+import Category from './ui/Category';
+
+
 export default function Categories() {
-    return (
-      <div>
-        Categories
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    setCategories(categoryData);
+
+  }, [])
+
+  return (
+
+
+
+    <div className="bg-white py-4">
+      <div className="container mx-auto max-w-[1233px]">
+        <h3 className="text-sm font-semibold mb-3">Kategoriler</h3>
+        <div className="grid grid-cols-10">
+          {categories && categories.map((category,index ) => <Category key={index} category={category} />)}
+        </div>
       </div>
-    )
-  }
-  
+
+    </div>
+  )
+}
